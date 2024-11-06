7d5540be28a758b4db1fd2e994f6e32c2ea2421c
BlindingDwarf <michaladamczuk5@gmail.com>
2024-11-06 12:25:20 +0100

    New commit

---
+++ helpers/data.js
@@ -0,0 +1,2 @@
+export const userId = "3da3ad89-a613-4dd2-8640-ebe2646008d0"
+export const baseUrl = "https://demoqa.com"
---
+++ tests/api.test.js
@@ -0,0 +1,29 @@
+import {expect} from "chai"
+import pkg from "pactum";
+const {spec} = pkg;
+import 'dotenv/config';
+import { baseUrl, userId } from "../helpers/data.js";
+
+describe("Api tests", () => {
+    it("get request", async () => {
+        const response = await spec().get(`${baseUrl}/BookStore/v1/Books`)
+        .inspect()
+        const responseB = JSON.stringify(response.body);
+        console.log("is dotenv work ?" + " " + process.env.SECRET_PASSWORD);
+        expect(response.statusCode).to.eql(200);
+        expect(responseB).to.include("Learning JavaScript Design Patterns");
+        expect(responseB).to.include("Eloquent JavaScript, Second Edition");
+
+    })
+    
+    it.skip("Create a user", async () => {
+        const response = await spec()
+        .post("https://demoqa.com/Account/v1/User")
+        .withBody({
+            userName: "Mihca",
+            password: process.env.SECRET_PASSWORD,
+        })
+        .inspect();
+        expect(response.statusCode).to.eql(201)
+    })
+})
